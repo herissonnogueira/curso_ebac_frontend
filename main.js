8cfbf9c759b278728a61bc80ca452b95f31190b5
Herisson Nogueira <herisson.nogueira002@gmail.com>
2023-10-21 20:17:17 -0300

    exercício html com javascript

--- main.js
+++ main.js
@@ -1,72 +1,30 @@
-const carroDoHerisson = {
-    modelo: 'Fiesta',
-    fabricante: 'Ford',
-    anoModelo: 2020,
-    anoFabricacao: 2019,
-    acelular: function() {
-        console.log("vrum");
+const form = document.getElementById('form');
+const saldo = document.getElementById('saldo');
+const saque = document.getElementById('saque');
+const acerto = document.getElementById('saquerealizado');
+const erro = document.getElementById('erro');
+
+function sacandoDinheiro(saldo, saque){
+    return saldo > saque;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let validarForm = sacandoDinheiro(saldo.valueAsNumber, saque.valueAsNumber)
+    if(validarForm){
+        acerto.style.display = 'block';
+        saldo.value='';
+        saque.value='';
     }
-}
+})
 
-const carroDaMaria = {
-    modelo: 'ka',
-    fabricante: 'Ford',
-    anoModelo: 2021,
-    anoFabricacao: 2020,
-    acelular: function() {
-        console.log("vrum");
+saque.addEventListener('keyup', function(e){
+    let validarForm = sacandoDinheiro(saldo.valueAsNumber, saque.valueAsNumber)
+    if(!validarForm){
+        erro.style.display = 'block';
+        acerto.style.display = 'none';
+    } else{
+        erro.style.display = 'none';
     }
-}
-
-function Carro(modelo, fabricante, anoModelo, anoFabricacao) {
-    this.modelo = modelo;
-    this.fabricante = fabricante;
-    this.anoModelo = anoFabricacao
-    this.acelerar = function() {
-        console.log("acelerar");
-    }
-}
-
-const carroDoHerisson2 = new Carro("Fiesta", "Ford", 2020, 2019);
-const carroDaMaria2 = new Carro("ka", "Ford", 2021, 2020);
-
-console.log(carroDoHerisson2);
-console.log(carroDaMaria2);
-
-const nome = "Herisson"
-const idade = "19"
-const ehMaiorDeIdade = true
-const conhecimentos = ["html", "css", "javascript"]
-
-const pessoa = {
-    nome: nome,
-    idade: idade,
-    ehMaiorDeIdade: ehMaiorDeIdade,
-    conhecimentos: conhecimentos,
-}
-
-function exibeAtributo(nomeAtributo) {
-    console.log(pessoa[nomeAtributo])
-}
-
-exibeAtributo('nome');
-
-pessoa['sobrenome'] = undefined;
-
-Object.freeze(pessoa);
-
-pessoa.nome = 'joao';
-
-if (pessoa['sobrenome']) {
-    console.log("a pessoa tem um sobrenome")
-}
-
-if ('sobrenome' in pessoa) {
-    console.log("tem sobrenome");
-}
-
-console.log(pessoa.nome)
-console.log(pessoa['nome'])
-
-console.log(Object.keys(pessoa).length);
-console.log(Object.values(pessoa));
+})
